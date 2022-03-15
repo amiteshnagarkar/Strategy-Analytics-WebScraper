568facf689e1fdd1605f5f581bf3075dd47999a0
amiteshnagarkar <amiteshnagarkar@gmail.com>
2022-03-15 13:19:42 +0000

    selenium and chromedriver installed, opens webpage

--- SkillTestScript.py
+++ SkillTestScript.py
@@ -6,30 +6,34 @@ import pandas as pd
 from datetime import date
 from datetime import datetime
 
-#Only EE for now.
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
+PATH = Service("/Users/amiteshnagarkar/Python/SA-Skills-Test/driver/chromedriver")
+
+
+#Only EE and iPhone for Now.
+
+#gets iPhone Data
 def scrapePayMonthlyPhoneData():
   print('Scraping Phone Data..hold on...');
-
   nowTime = datetime.now()
   date_time_string = nowTime.strftime("%d/%m/%Y %H:%M:%S")
   pay_monthly_name = []
   pay_monthly_plan_length = []
-
   #automate these url collection
   IphoneSE5G_url1 = f"https://shop.ee.co.uk/mobile-phones/pay-monthly/iphone-se-5g/details"
-  #SamsungGs22_url2 = f"https://shop.ee.co.uk/mobile-phones/pay-monthly/samsung-galaxy-s22-ultra-5g/details"
-  #SamsungGs22Plus_url2 = f"https://shop.ee.co.uk/mobile-phones/pay-monthly/samsung-galaxy-s22-plus-5g/details?color=PinkGold&capacity=128GB"
-
   response = requests.get(IphoneSE5G_url1)
   htmlContent = response.content
   soup = BeautifulSoup(htmlContent, "html.parser")
 
+  #device name
   for h1Tag in soup.find_all("h1", class_="h3 font-rubrik text-truncate-lines-5 text-left-md text-center"):
         pay_monthly_name.append(h1Tag.get_text(strip=True))
         bracket_removed_device_name=(','.join(pay_monthly_name))
         #return pay_monthly_name;
 
+  #plan length
   for pTag in soup.find_all("p", class_="margin-bottom-3 colour-slate"):
         pay_monthly_plan_length.append(pTag.get_text(strip=True))
         bracket_removed_plan_length_name=(','.join(pay_monthly_plan_length))
@@ -39,6 +43,12 @@ def scrapePayMonthlyPhoneData():
         print("Device Name: ",bracket_removed_device_name);
         print(s);
 
+def urlscraper():
+    print ("hello");
+    driver = webdriver.Chrome(service=PATH)
+    driver.get("https://shop.ee.co.uk/gallery/pay-monthly/mobile-phones-h00001?search=%3AofferingsOrder%3AproductBrands%3Abrands_apple")
+
 
 if __name__ == "__main__":
-  scrapePayMonthlyPhoneData();
+    urlscraper();
+    #scrapePayMonthlyPhoneData();
