e7a8fc338914d2802451adde35e62feefcc5ddb9
amiteshnagarkar <amiteshnagarkar@gmail.com>
2022-03-16 12:47:55 +0000

    finally can get all iphone data into list

--- O2_Script.py
+++ O2_Script.py
@@ -12,6 +12,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import time
+
 PATH = Service("/Users/amiteshnagarkar/Python/SA-Skills-Test/driver/chromedriver")
 
 #block images and javascript requests to speed things up.
@@ -22,7 +24,7 @@ chrome_prefs = {
     }
 }
 
-
+#change these to css selectors as they work best
 def iPhoneScraper():
     print ("Scraping O2 for iphones...")
     o2_website = "https://www.o2.co.uk/iphone"
@@ -59,37 +61,113 @@ def iPhoneScraper():
     driver.implicitly_wait(3)
    
     #this loop should collect all info needed for iPhones
+    phone_name_list = []
     for phones in range(len(xpath_list)):
 
         driver.implicitly_wait(3)
         #loop this for different phones
         link = driver.find_element(By.XPATH, xpath_list[phones])
         link.click()
+        driver.implicitly_wait(3)
+        #go to all drop down if possible?
 
-        #go to checkout with this specific phone
+        #click choose this plan with this specific phone
+        link = driver.find_element(By.CSS_SELECTOR, 'button.tne-secondary')
+        link.click()
+        driver.implicitly_wait(5)
 
-        #OR
-
-        #grab all data from this specifc phone index page and store it somewhere, also take screenshot here
-
-
-
-
-
+        driver.maximize_window() # For maximizing window
         
+
+
+        #choose later
+        #driver.execute_script("window.scrollTo(0, 1000);")
+        link = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div[2]/div/div[3]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div/button')
+        #driver.execute_script("window.scrollTo(0, window.scrollY + 1000)")
+
+        driver.execute_script("scrollBy(0,-500);")
+        time.sleep(2)
+
+        driver.execute_script("scrollBy(0,400);")
+        time.sleep(2)
+
+
+
+        #driver.execute_script("arguments[0].scrollIntoView();",link)
+        #link.click()
+        driver.execute_script("arguments[0].click();", link)
+        driver.implicitly_wait(5)
+
+        #Confirm spend cap
+        link = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div[2]/div/div[4]/div/div[2]/div/div/div[2]/div/div/div[6]/div/div/div/button/span')
+        driver.execute_script("arguments[0].scrollIntoView();",link)
+        #link.click()
+        driver.execute_script("arguments[0].click();", link)
+        driver.implicitly_wait(6)
+
+        #No extras
+        link = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div[2]/div/div[5]/div/div[2]/div/div/div[2]/div/div/div[1]/div[5]/div/div[2]')
+        driver.implicitly_wait(5)
+        driver.execute_script("arguments[0].scrollIntoView();",link)
+        driver.implicitly_wait(5)
+        #link.click()
+        driver.execute_script("arguments[0].click();", link)
+        driver.implicitly_wait(5)
+
+        #No accessories
+        link = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div[2]/div/div[5]/div/div[2]/div/div/div[2]/div/div/div[1]/div[5]/div/div[2]/button/div[1]')
+        driver.execute_script("arguments[0].scrollIntoView();",link)
+        #link.click()
+        driver.execute_script("arguments[0].click();", link)
+        driver.implicitly_wait(5)
+
+        print("not gone to basket yet")
+        #go to basket
+        link = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div[2]/div/div[8]/div[2]/div/div/div/div/button/span')
+        driver.execute_script("arguments[0].scrollIntoView();",link)
+        #link.click()
+        driver.execute_script("arguments[0].click();", link)
+        driver.implicitly_wait(5)
+
+        print("going to basket")
+
+        #grab data
+        phone_name = driver.find_element(By.CSS_SELECTOR, 'div.card-title').text
+        driver.implicitly_wait(2)
+        phone_model_and_month_plan = driver.find_element(By.CSS_SELECTOR, 'div.card-sentence').text
+        driver.implicitly_wait(2)
+        phone_upfront_cost = driver.find_element(By.CSS_SELECTOR, 'div.upfront-cost.ng-binding.ng-isolate-scope').text
+        driver.implicitly_wait(2)
+        phone_monthly_cost = driver.find_element(By.CSS_SELECTOR, 'div.upfront-cost.ng-binding.ng-isolate-scope').text
+        driver.implicitly_wait(2)
+        phone_tariff = driver.find_element(By.CSS_SELECTOR, 'div.card-sentence').text
+        driver.implicitly_wait(2)
+        phone_first_offer = driver.find_element(By.CSS_SELECTOR, 'p.offer-description.offer-text.activate-overlay.dynamic-overlay').text
+
+        #add to list
+        phone_name_list.append([phone_name, phone_model_and_month_plan, phone_upfront_cost, phone_monthly_cost, phone_tariff, phone_first_offer])
 
 
 
         driver.back()
         driver.implicitly_wait(3)
+        #OR
 
+        #grab all data from this specifc phone index page and store it somewhere, also take screenshot here
+        driver.back()
+        
+
+        driver.implicitly_wait(3)
+        driver.back()
+        driver.implicitly_wait(3)
+        driver.back()
         driver.implicitly_wait(3)
         #goes to see all page again
         link = driver.find_element(By.XPATH, show_all_xpath)
         link.click()
         driver.implicitly_wait(3)
 
-    
+    print (phone_name_list)
     driver.quit();
 
 
